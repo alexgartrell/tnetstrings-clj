7de3038e8b97f2937a17c990ac0f75fa82c3eac5
Alex Gartrell <alexgartrell@gmail.com>
2011-04-17 20:00:04 -0400

    Updated project information

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject tnetstrings "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject tnetstrings "0.1.0"
+  :description "tnetstrings in clojure www.tnetstrings.org"
   :dependencies [[org.clojure/clojure "1.2.1"]])
